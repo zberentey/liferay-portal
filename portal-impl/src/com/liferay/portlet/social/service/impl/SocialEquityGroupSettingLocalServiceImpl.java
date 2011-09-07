a8fa0c7b05693e7c9b3cca42c848c13b34362f81
Zsolt Berentey <zsolt.berentey@liferay.com>
2011-09-07 16:33:22 +0200

    LRDOCS-164 Javadoc for SocialEquityGroupSetting service

--- portal-impl/src/com/liferay/portlet/social/service/impl/SocialEquityGroupSettingLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/social/service/impl/SocialEquityGroupSettingLocalServiceImpl.java
@@ -23,11 +23,29 @@ import com.liferay.portlet.social.model.SocialEquitySettingConstants;
 import com.liferay.portlet.social.service.base.SocialEquityGroupSettingLocalServiceBaseImpl;
 
 /**
+ * The social equity group settings local service.
+ *
+ * <p>
+ * This service is responsible for storing social equity settings related to
+ * groups, i.e. whether social equity is turned on or off for a particular
+ * group.
+ * </p>
+ *
  * @author Zsolt Berentey
  */
 public class SocialEquityGroupSettingLocalServiceImpl
 	extends SocialEquityGroupSettingLocalServiceBaseImpl {
 
+	/**
+	 * Returns <code>true</code> if social equity is turned on for a model
+	 * (asset type) in a group.
+	 *
+	 * @param  groupId the primary key of the group
+	 * @param  className the class name for the target asset
+	 * @return <code>true</code> if social equity is enabled for the model;
+	 *         <code>false</code> otherwise
+	 * @throws SystemException if a system exception occurred
+	 */
 	public boolean isEnabled(long groupId, String className)
 		throws SystemException {
 
@@ -45,6 +63,19 @@ public class SocialEquityGroupSettingLocalServiceImpl
 		}
 	}
 
+	/**
+	 * Returns <code>true</code> if a certain type of social equity score is
+	 * turned on for a model (asset type) in a group.
+	 *
+	 * @param  groupId the primary key of the group
+	 * @param  className the class name for the target asset
+	 * @param  type the social equity score type, acceptable values are {@link
+	 *         SocialEquitySettingConstants.TYPE_INFORMATION}, {@link
+	 *         SocialEquitySettingConstants.TYPE_PARTICIAPTION}
+	 * @return <code>true</code> if the given type of social equity scoring is
+	 *         enabled for the model; <code>false</code> otherwise
+	 * @throws SystemException if a system exception occurred
+	 */
 	public boolean isEnabled(long groupId, String className, int type)
 		throws SystemException {
 
@@ -65,6 +96,24 @@ public class SocialEquityGroupSettingLocalServiceImpl
 		return true;
 	}
 
+	/**
+	 * Updates the group related social equity settings for a group and model.
+	 *
+	 * <p>
+	 * This method stores whether a particular type of social equity scoring
+	 * (information or participation) is turned on for a certain model in a
+	 * group.
+	 * </p>
+	 *
+	 * @param  groupId the primary key of the group
+	 * @param  className the class name for the target asset
+	 * @param  type the social equity score type, acceptable values are {@link
+	 *         SocialEquitySettingConstants.TYPE_INFORMATION}, {@link
+	 *         SocialEquitySettingConstants.TYPE_PARTICIAPTION}
+	 * @param  enabled whether social equity is turned on
+	 * @throws PortalException if the group could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void updateEquityGroupSetting(
 			long groupId, String className, int type, boolean enabled)
 		throws PortalException, SystemException {
