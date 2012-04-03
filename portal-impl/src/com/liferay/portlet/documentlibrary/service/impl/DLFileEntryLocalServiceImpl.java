0d788bfa59b6d687d3fc55ddfc58bf6730d41ab1
Manuel de la Peña <manuel.delapenya@liferay.com>
2012-04-03 13:22:57 +0200

    LPS-26339 Add trash services to DLFileEntry

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -73,6 +73,7 @@ import com.liferay.portlet.dynamicdatamapping.storage.Fields;
 import com.liferay.portlet.dynamicdatamapping.storage.StorageEngineUtil;
 import com.liferay.portlet.expando.model.ExpandoBridge;
 import com.liferay.portlet.expando.model.ExpandoColumnConstants;
+import com.liferay.portlet.trash.model.TrashEntry;
 
 import java.awt.image.RenderedImage;
 
@@ -100,6 +101,7 @@ import java.util.Map;
  * @author Brian Wing Shun Chan
  * @author Harry Mark
  * @author Alexander Chow
+ * @author Manuel de la Peña
  */
 public class DLFileEntryLocalServiceImpl
 	extends DLFileEntryLocalServiceBaseImpl {
@@ -880,6 +882,43 @@ public class DLFileEntryLocalServiceImpl
 
 			return dlFileEntryTypeLocalService.updateFileEntryFileEntryType(
 				dlFileEntry, serviceContext);
+		}
+		finally {
+			if (!isFileEntryCheckedOut(fileEntryId)) {
+				unlockFileEntry(fileEntryId);
+			}
+		}
+	}
+
+	public TrashEntry moveToTrash(
+			long userId, long fileEntryId, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		if (!hasFileEntryLock(userId, fileEntryId)) {
+			lockFileEntry(userId, fileEntryId);
+		}
+
+		try {
+			DLFileEntry dlFileEntry = getDLFileEntry(fileEntryId);
+
+			DLFileVersion dlFileVersion = dlFileEntry.getFileVersion();
+
+			// delete workflow
+
+			// create trash entry
+
+			TrashEntry trashEntry = trashEntryLocalService.addTrashEntry(
+				dlFileEntry.getCompanyId(), dlFileEntry.getGroupId(),
+				DLFileEntry.class.getName(), dlFileEntry.getPrimaryKey(),
+				dlFileVersion.getStatus(), null);
+
+			// modify status of current file version
+
+			dlFileVersion.setStatus(WorkflowConstants.STATUS_DELETED);
+
+			dlFileVersionPersistence.update(dlFileVersion, true);
+
+			return trashEntry;
 		}
 		finally {
 			if (!isFileEntryCheckedOut(fileEntryId)) {
