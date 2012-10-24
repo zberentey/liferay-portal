1a38722e8e56b64e94fca0d83f32cbdf1a88b9a6
LeventeHudak <levente.hudak@liferay.com>
2012-10-24 09:20:11 +0200

    LPS-29962 Support for moving web content to the Recycle Bin - Impls.

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleServiceImpl.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.theme.ThemeDisplay;
@@ -37,6 +38,7 @@ import java.util.Map;
 /**
  * @author Brian Wing Shun Chan
  * @author Raymond Augé
+ * @author Levente Hudák
  */
 public class JournalArticleServiceImpl extends JournalArticleServiceBaseImpl {
 
@@ -295,8 +297,9 @@ public class JournalArticleServiceImpl extends JournalArticleServiceBaseImpl {
 			OrderByComparator obc)
 		throws SystemException {
 
-		return journalArticlePersistence.filterFindByG_U_C(
-			groupId, userId, classNameId, start, end, obc);
+		return journalArticlePersistence.filterFindByG_U_C_NotS(
+			groupId, userId, classNameId, WorkflowConstants.STATUS_IN_TRASH,
+			start, end, obc);
 	}
 
 	public int getArticlesCount(long groupId, long folderId)
@@ -321,8 +324,8 @@ public class JournalArticleServiceImpl extends JournalArticleServiceBaseImpl {
 			long groupId, long userId, long classNameId)
 		throws SystemException {
 
-		return journalArticlePersistence.filterCountByG_U_C(
-			groupId, userId, classNameId);
+		return journalArticlePersistence.filterCountByG_U_C_NotS(
+			groupId, userId, classNameId, WorkflowConstants.STATUS_IN_TRASH);
 	}
 
 	public JournalArticle getDisplayArticleByUrlTitle(
@@ -397,6 +400,17 @@ public class JournalArticleServiceImpl extends JournalArticleServiceBaseImpl {
 		}
 	}
 
+	public JournalArticle moveArticleToTrash(
+			long userId, long groupId, String articleId)
+		throws PortalException, SystemException {
+
+		JournalArticlePermission.check(
+			getPermissionChecker(), groupId, articleId, ActionKeys.DELETE);
+
+		return journalArticleLocalService.moveArticleToTrash(
+			userId, groupId, articleId);
+	}
+
 	public void removeArticleLocale(long companyId, String languageId)
 		throws PortalException, SystemException {
 
@@ -419,6 +433,19 @@ public class JournalArticleServiceImpl extends JournalArticleServiceBaseImpl {
 
 		return journalArticleLocalService.removeArticleLocale(
 			groupId, articleId, version, languageId);
+	}
+
+	public void restoreArticleFromTrash(long groupId, String articleId)
+		throws PortalException, SystemException {
+
+		JournalArticle article = getLatestArticle(
+			groupId, articleId, WorkflowConstants.STATUS_IN_TRASH);
+
+		JournalArticlePermission.check(
+			getPermissionChecker(), article, ActionKeys.DELETE);
+
+		journalArticleLocalService.restoreArticleFromTrash(
+			getUserId(), article);
 	}
 
 	public List<JournalArticle> search(
