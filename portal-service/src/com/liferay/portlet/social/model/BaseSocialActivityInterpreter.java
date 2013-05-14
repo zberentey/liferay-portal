1b876cd6407facfb17d56160d15a4d0044ff6d26
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-05-13 19:13:47 -0700

    LPS-35229 Add a thread local for the actual entity so that interpreters don't have to go to a service multiple times during the interpret cycle

--- portal-service/src/com/liferay/portlet/social/model/BaseSocialActivityInterpreter.java
+++ portal-service/src/com/liferay/portlet/social/model/BaseSocialActivityInterpreter.java
@@ -65,10 +65,15 @@ public abstract class BaseSocialActivityInterpreter
 		SocialActivity activity, ServiceContext serviceContext) {
 
 		try {
+			_entityThreadLocal.set(doGetEntity(activity, serviceContext));
+
 			return doInterpret(activity, serviceContext);
 		}
 		catch (Exception e) {
 			_log.error("Unable to interpret activity", e);
+		}
+		finally {
+			_entityThreadLocal.remove();
 		}
 
 		return null;
@@ -86,11 +91,16 @@ public abstract class BaseSocialActivityInterpreter
 			if (!activities.isEmpty()) {
 				SocialActivity activity = activities.get(0);
 
+				_entityThreadLocal.set(doGetEntity(activity, serviceContext));
+
 				return doInterpret(activity, serviceContext);
 			}
 		}
 		catch (Exception e) {
 			_log.error("Unable to interpret activity set", e);
+		}
+		finally {
+			_entityThreadLocal.remove();
 		}
 
 		return null;
@@ -135,6 +145,13 @@ public abstract class BaseSocialActivityInterpreter
 	 */
 	protected String cleanContent(String content) {
 		return StringUtil.shorten(HtmlUtil.extractText(content), 200);
+	}
+
+	protected Object doGetEntity(
+			SocialActivity activity, ServiceContext serviceContext)
+		throws SystemException {
+
+		return null;
 	}
 
 	protected SocialActivityFeedEntry doInterpret(
@@ -193,6 +210,10 @@ public abstract class BaseSocialActivityInterpreter
 		throws Exception {
 
 		return StringPool.BLANK;
+	}
+
+	protected Object getEntity() {
+		return _entityThreadLocal.get();
 	}
 
 	protected String getEntryTitle(
@@ -321,7 +342,7 @@ public abstract class BaseSocialActivityInterpreter
 
 		long classPK = activity.getClassPK();
 
-		if ((trashHandler != null) &&
+		if ((trashHandler != null) && (getEntity() != null) &&
 			(trashHandler.isInTrash(classPK) ||
 			 trashHandler.isInTrashContainer(classPK))) {
 
@@ -337,7 +358,7 @@ public abstract class BaseSocialActivityInterpreter
 
 		String path = getPath(activity, serviceContext);
 
-		if (Validator.isNull(path)) {
+		if (Validator.isNull(path) || (getEntity() == null)) {
 			return null;
 		}
 
@@ -526,5 +547,8 @@ public abstract class BaseSocialActivityInterpreter
 
 	private SocialActivityFeedEntry _deprecatedMarkerSocialActivityFeedEntry =
 		new SocialActivityFeedEntry(StringPool.BLANK, StringPool.BLANK);
+
+	private final ThreadLocal<Object> _entityThreadLocal =
+		new ThreadLocal<Object>();
 
 }
