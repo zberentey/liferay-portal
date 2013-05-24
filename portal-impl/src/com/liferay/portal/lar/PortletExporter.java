0e117be539819c344798e17260d0897ef9a4af46
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-05-24 13:15:30 +0200

    LPS-35598 Change the summary format in manifest.xml and populate ManifestSummary during export

--- portal-impl/src/com/liferay/portal/lar/PortletExporter.java
+++ portal-impl/src/com/liferay/portal/lar/PortletExporter.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.lar.ExportImportPathUtil;
 import com.liferay.portal.kernel.lar.ExportImportThreadLocal;
 import com.liferay.portal.kernel.lar.ExportImportUtil;
+import com.liferay.portal.kernel.lar.ManifestSummary;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.lar.PortletDataContextFactoryUtil;
 import com.liferay.portal.kernel.lar.PortletDataHandler;
@@ -874,6 +875,12 @@ public class PortletExporter {
 				_log.warn("Unable to export deletion.", je);
 			}
 		}
+
+		ManifestSummary manifestSummary =
+			portletDataContext.getManifestSummary();
+
+		manifestSummary.incrementDeletionCount(
+			PortalUtil.getClassName(deleteActivity.getClassNameId()));
 	}
 
 	protected void exportDeletions(final PortletDataContext portletDataContext)
