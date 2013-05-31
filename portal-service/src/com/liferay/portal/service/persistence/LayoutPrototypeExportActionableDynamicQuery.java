1e1524789b89e53a43bb2ebba0eee54e1b03161c
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-05-31 15:33:17 +0200

    LPS-35667 Regenerate *ExportActionableDynamicQuery

--- portal-service/src/com/liferay/portal/service/persistence/LayoutPrototypeExportActionableDynamicQuery.java
+++ portal-service/src/com/liferay/portal/service/persistence/LayoutPrototypeExportActionableDynamicQuery.java
@@ -17,6 +17,7 @@ package com.liferay.portal.service.persistence;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.ManifestSummary;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.portal.model.LayoutPrototype;
@@ -33,8 +34,24 @@ public class LayoutPrototypeExportActionableDynamicQuery
 	}
 
 	@Override
+	@SuppressWarnings("unused")
+	public long performCount() throws PortalException, SystemException {
+		long count = super.performCount();
+
+		ManifestSummary manifestSummary = _portletDataContext.getManifestSummary();
+
+		manifestSummary.addModelCount(getManifestSummaryKey(), count);
+
+		return count;
+	}
+
+	@Override
 	protected void addCriteria(DynamicQuery dynamicQuery) {
 		_portletDataContext.addDateRangeCriteria(dynamicQuery, "modifiedDate");
+	}
+
+	protected String getManifestSummaryKey() {
+		return LayoutPrototype.class.getName();
 	}
 
 	@Override
