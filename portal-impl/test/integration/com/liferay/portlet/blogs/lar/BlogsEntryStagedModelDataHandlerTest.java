cf5bf09c050e74b3933749b890730a035e647209
KocsisDaniel <daniel.kocsis@liferay.com>
2013-06-21 10:18:02 +0200

    LPS-36216 Integration Tests - stage model data handler tests (part 2)

--- portal-impl/test/integration/com/liferay/portlet/blogs/lar/BlogsEntryStagedModelDataHandlerTest.java
+++ portal-impl/test/integration/com/liferay/portlet/blogs/lar/BlogsEntryStagedModelDataHandlerTest.java
@@ -15,6 +15,7 @@
 package com.liferay.portlet.blogs.lar;
 
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
 import com.liferay.portal.lar.BaseStagedModelDataHandlerTestCase;
 import com.liferay.portal.model.Group;
@@ -54,6 +55,23 @@ public class BlogsEntryStagedModelDataHandlerTest
 	}
 
 	@Override
+	protected void deleteStagedModel(
+			StagedModel stagedModel,
+			Map<String, List<StagedModel>> dependentStagedModelsMap,
+			Group group)
+		throws Exception {
+
+		BlogsEntryLocalServiceUtil.deleteEntry((BlogsEntry)stagedModel);
+	}
+
+	@Override
+	protected StagedModelType[] getDeletionSystemEventModelTypes() {
+		BlogsPortletDataHandler blogsPortletDataHandler =
+			new BlogsPortletDataHandler();
+
+		return blogsPortletDataHandler.getDeletionSystemEventModelTypes();
+	}
+
 	@Override
 	protected StagedModel getStagedModel(String uuid, Group group)
 		throws SystemException {
