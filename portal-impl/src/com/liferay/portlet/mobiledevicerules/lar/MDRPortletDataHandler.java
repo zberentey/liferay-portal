634aaa2734403f4fa3efc2bdc54c59a80042533c
KocsisDaniel <daniel.kocsis@liferay.com>
2013-06-25 11:30:45 +0200

    LPS-36216 Add missing referrers to export/counter queries

--- portal-impl/src/com/liferay/portlet/mobiledevicerules/lar/MDRPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/mobiledevicerules/lar/MDRPortletDataHandler.java
@@ -21,6 +21,8 @@ import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.portal.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.mobiledevicerules.model.MDRAction;
 import com.liferay.portlet.mobiledevicerules.model.MDRRule;
 import com.liferay.portlet.mobiledevicerules.model.MDRRuleGroup;
@@ -48,8 +50,11 @@ public class MDRPortletDataHandler extends BasePortletDataHandler {
 	public MDRPortletDataHandler() {
 		setDeletionSystemEventModelTypes(
 			new StagedModelType(MDRAction.class),
+			new StagedModelType(MDRAction.class, Layout.class),
+			new StagedModelType(MDRRule.class),
 			new StagedModelType(MDRRuleGroup.class),
-			new StagedModelType(MDRRuleGroupInstance.class));
+			new StagedModelType(MDRRuleGroupInstance.class),
+			new StagedModelType(MDRRuleGroupInstance.class, Layout.class));
 		setExportControls(
 			new PortletDataHandlerBoolean(
 				NAMESPACE, "rules", true, false, null, MDRRule.class.getName()),
@@ -98,7 +103,15 @@ public class MDRPortletDataHandler extends BasePortletDataHandler {
 
 		if (portletDataContext.getBooleanParameter(NAMESPACE, "actions")) {
 			ActionableDynamicQuery actionsActionableDynamicQuery =
-				new MDRActionExportActionableDynamicQuery(portletDataContext);
+				new MDRActionExportActionableDynamicQuery(portletDataContext) {
+
+				@Override
+				protected StagedModelType getStagedModelType() {
+					return new StagedModelType(
+						PortalUtil.getClassNameId(MDRAction.class), -1);
+				}
+
+			};
 
 			actionsActionableDynamicQuery.performActions();
 		}
@@ -149,6 +162,19 @@ public class MDRPortletDataHandler extends BasePortletDataHandler {
 		throws Exception {
 
 		ActionableDynamicQuery actionsActionableDynamicQuery =
+			new MDRActionExportActionableDynamicQuery(portletDataContext) {
+
+			@Override
+			protected StagedModelType getStagedModelType() {
+				return new StagedModelType(
+					MDRAction.class.getName(), Layout.class.getName());
+			}
+
+		};
+
+		actionsActionableDynamicQuery.performCount();
+
+		actionsActionableDynamicQuery =
 			new MDRActionExportActionableDynamicQuery(portletDataContext);
 
 		actionsActionableDynamicQuery.performCount();
@@ -164,6 +190,20 @@ public class MDRPortletDataHandler extends BasePortletDataHandler {
 		ruleGroupsActionableDynamicQuery.performCount();
 
 		ActionableDynamicQuery ruleGroupInstancesActionableDynamicQuery =
+			new MDRRuleGroupInstanceExportActionableDynamicQuery(
+				portletDataContext) {
+
+				@Override
+				protected StagedModelType getStagedModelType() {
+					return new StagedModelType(
+						MDRRuleGroupInstance.class.getName(),
+						Layout.class.getName());
+				}
+			};
+
+		ruleGroupInstancesActionableDynamicQuery.performCount();
+
+		ruleGroupInstancesActionableDynamicQuery =
 			new MDRRuleGroupInstanceExportActionableDynamicQuery(
 				portletDataContext);
 
