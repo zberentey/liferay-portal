e1227682de11bfeab661ba4360e986cf08a72a73
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-06-26 12:51:56 +0200

    LPS-35256 Generated

--- portal-impl/src/com/liferay/portal/model/impl/SystemEventCacheModel.java
+++ portal-impl/src/com/liferay/portal/model/impl/SystemEventCacheModel.java
@@ -37,7 +37,7 @@ public class SystemEventCacheModel implements CacheModel<SystemEvent>,
 	Externalizable {
 	@Override
 	public String toString() {
-		StringBundler sb = new StringBundler(25);
+		StringBundler sb = new StringBundler(29);
 
 		sb.append("{systemEventId=");
 		sb.append(systemEventId);
@@ -61,6 +61,10 @@ public class SystemEventCacheModel implements CacheModel<SystemEvent>,
 		sb.append(referrerClassNameId);
 		sb.append(", type=");
 		sb.append(type);
+		sb.append(", eventSetId=");
+		sb.append(eventSetId);
+		sb.append(", parentSystemEventId=");
+		sb.append(parentSystemEventId);
 		sb.append(", extraData=");
 		sb.append(extraData);
 		sb.append("}");
@@ -103,6 +107,8 @@ public class SystemEventCacheModel implements CacheModel<SystemEvent>,
 
 		systemEventImpl.setReferrerClassNameId(referrerClassNameId);
 		systemEventImpl.setType(type);
+		systemEventImpl.setEventSetId(eventSetId);
+		systemEventImpl.setParentSystemEventId(parentSystemEventId);
 
 		if (extraData == null) {
 			systemEventImpl.setExtraData(StringPool.BLANK);
@@ -129,6 +135,8 @@ public class SystemEventCacheModel implements CacheModel<SystemEvent>,
 		classUuid = objectInput.readUTF();
 		referrerClassNameId = objectInput.readLong();
 		type = objectInput.readInt();
+		eventSetId = objectInput.readLong();
+		parentSystemEventId = objectInput.readLong();
 		extraData = objectInput.readUTF();
 	}
 
@@ -160,6 +168,8 @@ public class SystemEventCacheModel implements CacheModel<SystemEvent>,
 
 		objectOutput.writeLong(referrerClassNameId);
 		objectOutput.writeInt(type);
+		objectOutput.writeLong(eventSetId);
+		objectOutput.writeLong(parentSystemEventId);
 
 		if (extraData == null) {
 			objectOutput.writeUTF(StringPool.BLANK);
@@ -180,5 +190,7 @@ public class SystemEventCacheModel implements CacheModel<SystemEvent>,
 	public String classUuid;
 	public long referrerClassNameId;
 	public int type;
+	public long eventSetId;
+	public long parentSystemEventId;
 	public String extraData;
 }
