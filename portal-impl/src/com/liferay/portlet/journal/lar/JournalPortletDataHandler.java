e4365ebf88c1ae1919568303e702a03d90d62fbc
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-06-28 11:15:03 +0200

    LPS-36407 Update portlet data handlers

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandler.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.portal.kernel.lar.PortletDataHandlerControl;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.portal.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.util.PortalUtil;
@@ -88,11 +89,13 @@ public class JournalPortletDataHandler extends BasePortletDataHandler {
 	public static final String NAMESPACE = "journal";
 
 	public JournalPortletDataHandler() {
-		setDeletionSystemEventClassNames(
-			DDMStructure.class.getName(), DDMTemplate.class.getName(),
-			JournalArticle.class.getName(), JournalFeed.class.getName(),
-			JournalFolder.class.getName());
 		setDataLocalized(true);
+		setDeletionSystemEventModelTypes(
+			new StagedModelType(DDMStructure.class, JournalArticle.class),
+			new StagedModelType(DDMTemplate.class, DDMStructure.class),
+			new StagedModelType(JournalArticle.class),
+			new StagedModelType(JournalFeed.class),
+			new StagedModelType(JournalFolder.class));
 		setExportControls(
 			new PortletDataHandlerBoolean(
 				NAMESPACE, "web-content", true, false,
@@ -377,8 +380,8 @@ public class JournalPortletDataHandler extends BasePortletDataHandler {
 			}
 
 			@Override
-			protected String getManifestSummaryKey() {
-				return ManifestSummary.getManifestSummaryKey(
+			protected StagedModelType getStagedModelType() {
+				return new StagedModelType(
 					DDMStructure.class.getName(),
 					JournalArticle.class.getName());
 			}
@@ -428,8 +431,8 @@ public class JournalPortletDataHandler extends BasePortletDataHandler {
 			}
 
 			@Override
-			protected String getManifestSummaryKey() {
-				return ManifestSummary.getManifestSummaryKey(
+			protected StagedModelType getStagedModelType() {
+				return new StagedModelType(
 					DDMTemplate.class.getName(), DDMStructure.class.getName());
 			}
 
