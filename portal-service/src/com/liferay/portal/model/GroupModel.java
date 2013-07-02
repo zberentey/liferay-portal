7e1f55c473138c06b7efff90b3cc03a15a53b148
Michael Han <michael.han@liferay.com>
2013-07-02 09:16:13 -0500

    LPS-37252 regen svcs

--- portal-service/src/com/liferay/portal/model/GroupModel.java
+++ portal-service/src/com/liferay/portal/model/GroupModel.java
@@ -329,6 +329,20 @@ public interface GroupModel extends AttachedModel, BaseModel<Group> {
 	 */
 	public void setActive(boolean active);
 
+	/**
+	 * Returns the remote staging group count of this group.
+	 *
+	 * @return the remote staging group count of this group
+	 */
+	public int getRemoteStagingGroupCount();
+
+	/**
+	 * Sets the remote staging group count of this group.
+	 *
+	 * @param remoteStagingGroupCount the remote staging group count of this group
+	 */
+	public void setRemoteStagingGroupCount(int remoteStagingGroupCount);
+
 	@Override
 	public boolean isNew();
 
