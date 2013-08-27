b24241874346259c469cd1b963a7e3eec1cd2fbe
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-08-27 14:02:04 +0200

    LPS-39399 Add method to trash handlers to get the parent entity

--- portal-service/src/com/liferay/portal/kernel/trash/BaseTrashHandler.java
+++ portal-service/src/com/liferay/portal/kernel/trash/BaseTrashHandler.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.ClassedModel;
 import com.liferay.portal.model.ContainerModel;
+import com.liferay.portal.model.TrashedModel;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.ServiceContext;
@@ -60,7 +61,6 @@ public abstract class BaseTrashHandler implements TrashHandler {
 	}
 
 	@Override
-	@SuppressWarnings("unused")
 	public ContainerModel getContainerModel(long containerModelId)
 		throws PortalException, SystemException {
 
@@ -78,7 +78,6 @@ public abstract class BaseTrashHandler implements TrashHandler {
 	}
 
 	@Override
-	@SuppressWarnings("unused")
 	public List<ContainerModel> getContainerModels(
 			long classPK, long containerModelId, int start, int end)
 		throws PortalException, SystemException {
@@ -87,7 +86,6 @@ public abstract class BaseTrashHandler implements TrashHandler {
 	}
 
 	@Override
-	@SuppressWarnings("unused")
 	public int getContainerModelsCount(long classPK, long containerModelId)
 		throws PortalException, SystemException {
 
@@ -103,6 +101,24 @@ public abstract class BaseTrashHandler implements TrashHandler {
 	@SuppressWarnings("unused")
 	public ContainerModel getParentContainerModel(long classPK)
 		throws PortalException, SystemException {
+
+		return null;
+	}
+
+	@Override
+	public ContainerModel getParentContainerModel(TrashedModel trashedModel)
+		throws PortalException, SystemException {
+
+		if (trashedModel == null) {
+			return null;
+		}
+
+		if (trashedModel instanceof ContainerModel) {
+			ContainerModel containerModel = (ContainerModel)trashedModel;
+
+			return getContainerModel(
+				containerModel.getParentContainerModelId());
+		}
 
 		return null;
 	}
