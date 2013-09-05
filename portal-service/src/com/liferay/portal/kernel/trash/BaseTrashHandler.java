a9fe9e096a5d79147f9b764d8f69e852c2330c7c
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-09-05 13:40:25 +0200

    LPS-39399 Add isTrashEntry() and getTrashEntry() to trash handlers and TrashEntry

--- portal-service/src/com/liferay/portal/kernel/trash/BaseTrashHandler.java
+++ portal-service/src/com/liferay/portal/kernel/trash/BaseTrashHandler.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.ClassedModel;
 import com.liferay.portal.model.ContainerModel;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
@@ -27,6 +28,8 @@ import com.liferay.portlet.asset.AssetRendererFactoryRegistryUtil;
 import com.liferay.portlet.asset.model.AssetRenderer;
 import com.liferay.portlet.asset.model.AssetRendererFactory;
 import com.liferay.portlet.trash.model.TrashEntry;
+
+import java.io.Serializable;
 
 import java.util.Collections;
 import java.util.List;
@@ -201,6 +204,13 @@ public abstract class BaseTrashHandler implements TrashHandler {
 	}
 
 	@Override
+	public TrashEntry getTrashEntry(long classPK)
+		throws PortalException, SystemException {
+
+		return null;
+	}
+
+	@Override
 	public TrashRenderer getTrashRenderer(long classPK)
 		throws PortalException, SystemException {
 
@@ -274,6 +284,33 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		throws PortalException, SystemException {
 
 		return true;
+	}
+
+	@Override
+	public boolean isTrashEntry(
+		TrashEntry trashEntry, ClassedModel classedModel) {
+
+		if ((trashEntry == null) || (classedModel == null)) {
+			return false;
+		}
+
+		String className = getClassName();
+
+		if (!className.equals(trashEntry.getClassName())) {
+			return false;
+		}
+
+		Serializable primaryKeyObj = classedModel.getPrimaryKeyObj();
+
+		if (!(primaryKeyObj instanceof Long)) {
+			return false;
+		}
+
+		if (trashEntry.getClassPK() == (Long)primaryKeyObj) {
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override
